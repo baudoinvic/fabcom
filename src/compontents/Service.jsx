cc755a3ef2d25b3db0d5a4193d60fee239ee2ee9
baudoin <baudoinvicbolingo@gmail.com>
2023-12-17 14:04:05 +0200

    five commit

--- src/compontents/Service.jsx
+++ src/compontents/Service.jsx
@@ -19,53 +19,7 @@ const Service = () => {
         </span>
       </div>
 
-      <div class="container mx-auto">
-        <div class="flex justify-center">
-          <span class="text-2xl font-bold">What we do</span>
-        </div>
-
-        <div class="flex flex-wrap -mx-4">
-          <div class="w-full py-8 md:w-1/2 lg:w-1/3 px-4 mb-8">
-            <div class="bg-white rounded-lg shadow-md hover:shadow-lg">
-              <div class="p-9">
-                <h2 class="text-xl font-semibold">Hardware Repairing</h2>
-                <p class="text-gray-600 mt-2">
-                  Automotive TSZ18x zero-drift, precision op amps for harsh
-                  environments and extended mission profiles<br></br> we are
-                  here to help you to Troubleshoot your pc and upgrade all
-                  software requirement
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full py-8 md:w-1/2 lg:w-1/3 px-4 mb-8">
-            <div class="bg-white rounded-lg shadow-md hover:shadow-lg">
-              <div class="p-9">
-                <h2 class="text-xl font-semibold">Automotive Repair</h2>
-                <p class="text-gray-600 mt-2">
-                  Automotive TSZ18x zero-drift, precision op amps for harsh
-                  environments and extended mission profiles<br></br> we are
-                  here to help you to Troubleshoot your pc and upgrade all
-                  software requirement
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="w-full py-8 md:w-1/2 lg:w-1/3 px-4 mb-8">
-            <div class="bg-white rounded-lg shadow-md hover:shadow-lg">
-              <div class="p-9">
-                <h2 class="text-xl font-semibold">Troubleshoot</h2>
-                <p class="text-gray-600 mt-2">
-                  Automotive TSZ18x zero-drift, precision op amps for harsh
-                  environments and extended mission profiles<br></br> we are
-                  here to help you to Troubleshoot your pc and upgrade all
-                  software requirement
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+    
     </div>
   );
 }
