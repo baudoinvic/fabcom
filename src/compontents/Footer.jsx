6c631d80c3901f98909814a4d8febfe11b88960e
baudoin <baudoinvicbolingo@gmail.com>
2024-01-12 19:49:15 +0200

    today last

--- src/compontents/Footer.jsx
+++ src/compontents/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -12,21 +13,33 @@ const Footer = () => {
                 <li>
                   <a href="">Home</a>
                 </li>
-                <li>
-                  <a href="Aboutus">About us</a>
-                </li>
-                <li>
-                  <a href="products">our products</a>
-                </li>
-                <li>
-                  <a href="careers">Careers</a>
-                </li>
+
+                <Link to="/About">
+                  <li>
+                    <a href="">About us</a>
+                  </li>
+                </Link>
+
+                <Link to="/products">
+                  <li>
+                    <a href="">our products</a>
+                  </li>
+                </Link>
+
+                <Link to="/career">
+                  <li>
+                    <a href="">Careers</a>
+                  </li>
+                </Link>
+
                 <li>
                   <a href="">Get in touch &amp; with us</a>
                 </li>
-                <li>
-                  <a href="ourservices">Our services</a>
-                </li>
+                <Link to="/ourservices">
+                  <li>
+                    <a href="">Our services</a>
+                  </li>
+                </Link>
               </ul>
             </nav>
           </div>
@@ -35,11 +48,14 @@ const Footer = () => {
             <h2 class="text-white font-bold mb-4">Connect with us</h2>
             <nav>
               <ul class="text-blue-500">
+                <Link to="/contact">
+                  <li>
+                    <a href="">Contact us</a>
+                  </li>
+                </Link>
+
                 <li>
-                  <a href="contact">Contact us</a>
-                </li>
-                <li>
-                  <a href="#">Contact  &amp; the owner </a>
+                  <a href="#">Contact &amp; the owner </a>
                 </li>
                 <li>
                   <a href="#">our offices</a>
@@ -56,7 +72,9 @@ const Footer = () => {
 
           <div class="w-full md:w-1/2 lg:w-1/3">
             <div id="subscriptionContainer" class="mb-8">
-              <h2 class="text-white font-bold mb-4">Subscribe to Our Newsletter</h2>
+              <h2 class="text-white font-bold mb-4">
+                Subscribe to Our Newsletter
+              </h2>
               <form action="#" class="subscription-form">
                 <div class="flex items-center">
                   <input
