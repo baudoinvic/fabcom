9b219b6304151a2ea9fecd0663e443e3627882e3
baudoin <baudoinvicbolingo@gmail.com>
2024-01-13 20:59:07 +0200

    responsive

--- src/compontents/About/About.jsx
+++ src/compontents/About/About.jsx
@@ -6,53 +6,37 @@ import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
-    <div className="">
+    <div class="">
       <Navbar />
-      <div className="relative">
+      <div class="relative">
         <img
-          class="w-full object-cover shadow-md "
+          class="w-full object-cover shadow-md"
           style={{ height: "560px" }}
           src="https://fablab.rw/wp-content/uploads/2022/09/fablab-scaled.jpg"
           alt="Description"
         />
 
-        <div
-          class="absolute inset-0 flex flex-col items-center justify-center "
-          style={{ marginRight: "40rem" }}
-        >
-          <span
-            class="font-bold text-white text-left mb-4 max-w-3/4 animate-moveText"
-            style={{ fontSize: "30px" }}
-          >
-            Welcome to FabTech Hub, <br></br>where innovation meets excellence
-            As a dynamic<br></br> and forward-thinking technology company
+        <div class="absolute inset-0 flex flex-col items-center justify-center md:justify-start text-center md:text-left p-8 md:p-16">
+          <span class="font-bold text-white mb-4 max-w-3/4 animate-moveText text-lg md:text-2xl lg:text-3xl">
+            Welcome to FabTech Hub, <br /> where innovation meets excellence. As
+            a dynamic <br /> and forward-thinking technology company
           </span>
 
           <Link to="/Ourservices">
-            <button
-              class="px-4 py-4 bg-[#02010130] text-white font-bold rounded hover:bg-black-100"
-              style={{
-                borderRadius: "30px",
-                border: "solid 1px gray",
-                marginRight: "25rem",
-              }}
-            >
+            <button class="px-4 py-4 bg-[#02010130] text-white font-bold rounded hover:bg-black-100 md:mr-16">
               See our services
             </button>
           </Link>
         </div>
       </div>
-      <div
-        class="flex flex-col md:flex-row "
-        style={{ marginRight: "3rem", marginLeft: "3rem", marginTop: "7rem" }}
-      >
-        <div class=" w-full md:w-1/2 p-8   ">
+      <div class="flex flex-col md:flex-row md:mr-3 md:ml-3 md:mt-7">
+        <div class="w-full md:w-1/2 p-8">
           <h1 class="text-gray-800 text-3xl font-bold mb-4">About us</h1>
-          <p class="mb-4">
+          <p class="mb-4 text-base md:text-lg lg:text-xl">
             Welcome to FabTech Hub, where innovation meets excellence! As a
-            dynamic and forward-thinking technology company, we specialised in
+            dynamic and forward-thinking technology company, we specialize in
             the seamless integration of hardware, software, and IoT solutions.
-            Our Vision: At FabTech Hub , we envision a connected world where
+            Our Vision: At FabTech Hub, we envision a connected world where
             cutting-edge technology enhances the way we live, work, and
             interact. Our goal is to be at the forefront of this transformation,
             driving innovation through the convergence of hardware, software,
@@ -64,7 +48,8 @@ const About = () => {
             our hardware stands out in terms of performance, durability, and
             user experience.
           </p>
-          <p class="mb-4">
+
+          <p className="mb-4 text-base md:text-lg lg:text-xl">
             Pioneering Software Development: Software is the heart and soul of
             any technological ecosystem, and at FabTech Hub, we take pride in
             our software development prowess. Our team of software engineers is
@@ -80,8 +65,6 @@ const About = () => {
             connectivity, data exchange, and automation. Our IoT solutions are
             geared towards enhancing efficiency, optimising processes, and
             providing valuable insights through real-time data analytics.
-          </p>
-          <p class="mb-4">
             Customer-Centric Approach: At the core of our company is a
             commitment to customer satisfaction. We work closely with our
             clients to understand their unique needs and challenges, tailoring
@@ -98,20 +81,10 @@ const About = () => {
             we continue to design the future through the seamless integration of
             hardware, software, and IoT at FabTech Hub.
           </p>
-       
         </div>
-        <div class="w-full  md:w-1/2" style={{ marginTop: "7rem" }}>
-          {/* <div className="video">
-            <video controls width="740" height="360">
-              <source
-                src="https://html.geekcodelab.com/holiday-planners/assets/images/highlight-video.mp4"
-                type="video/mp4"
-              />
-            </video>
-          </div> */}
-
-          <div>
-            <video controls width="full" height="560">
+        <div class="w-full md:w-1/2 mt-7">
+          <div class="aspect-w-16 aspect-h-9">
+            <video controls class="w-full h-full" width="full" height="560">
               <source src="/anima.mp4" type="video/mp4" />
             </video>
           </div>
