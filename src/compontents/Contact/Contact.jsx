9dd489b00e6db50b208e1d7f1a7e66f0c6f7b4db
baudoin <baudoinvicbolingo@gmail.com>
2024-01-20 20:04:18 +0200

    today update

--- src/compontents/Contact/Contact.jsx
+++ src/compontents/Contact/Contact.jsx
@@ -173,7 +173,7 @@ const Contact = () => {
           <ToastContainer />
         </div>
       </div>
-
+        
       <div className="items-center p-4 mt-10">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d30607.79514638295!2d30.066595630129367!3d-1.9550632439832505!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1ssp rwanda!5e0!3m2!1sen!2srw!4v1663256798141!5m2!1sen!2srw"
