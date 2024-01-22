c1385610facafa2904bb302f54328bd0da8907fc
baudoin <baudoinvicbolingo@gmail.com>
2024-01-22 20:31:35 +0200

    adding functionallity in apply form

--- src/compontents/Apply/Apply.jsx
+++ src/compontents/Apply/Apply.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../Footer';
 import Navbar from '../Navbar';
+import { ToastContainer,toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import axios from 'axios';
 
 const Apply = () => {
+
+  // const [formData, setFormData] = useState({
+  //   Firstname:'',
+  //   Lastname:'',
+  //   email:'',
+  //   phoneNumber:'',
+  //   file: null,
+  //   yearsOfExperience: 0,
+  //   description: '',
+  // });
+
+      const [formData, setFormData] = useState({
+        Firstname: "",
+        Lastname: "",
+        email: "",
+        phoneNumber: "",
+        file: "",
+        yearsOfExperience: "",
+        description: ""
+      });
+
+
+       const handleChange = (e) => {
+         const fieldName = e.target.name;
+
+         if (fieldName === "file") {
+           const selectedFile = e.target.files[0];
+
+           setFormData({
+             ...formData,
+             [fieldName]: selectedFile,
+           });
+         } else {
+           setFormData({
+             ...formData,
+             [fieldName]: e.target.value,
+           });
+         }
+       };
+
+
+
+
+     const handleSubmit = async (e) => {
+       e.preventDefault();
+
+       try{
+         let token = localStorage.getItem("Token");
+         console.log("Request Data", formData);
+
+
+         const response = await axios({
+            url: "https://fabtechhub.onrender.com/FabtechHub/application/newApplication",
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+       
+            data: JSON.stringify(formData)
+         });
+
+         console.log("Response Data:", response.data);
+         toast.success("your application was submitted successfully")
+       } catch (error){
+          console.error("Error:", error.response ? error.response.data : error);
+         toast.error("Failed to apply. Please try again later.");
+       }
+     };
+ 
+
+
+
+
+
   return (
     <div className="job-applications">
       <Navbar />
@@ -10,17 +88,34 @@ const Apply = () => {
         <div class="bg-white container  mx-auto p-8 max-w-md shadow-lg">
           <h2 class="text-2xl font-semibold mb-6">Job Application Form</h2>
 
-          <form action="/submit_application" method="post" class="space-y-4">
+          <form onClick={handleSubmit} action="/submit_application" method="post" class="space-y-4">
             <div class="flex flex-col">
               <label for="name" class="mb-1">
-                Full Name:
+                Firstname:
               </label>
               <input
                 type="text"
-                id="name"
-                name="name"
+                id="Firstname"
+                name="Firstname"
                 required
                 class="border p-2 rounded"
+              
+                onChange={handleChange}
+              ></input>
+            </div>
+
+            <div class="flex flex-col">
+              <label for="name" class="mb-1">
+                LastName:
+              </label>
+              <input
+                type="text"
+                id="Lastname"
+                name="Lastname"
+                required
+                class="border p-2 rounded"
+             
+                onChange={handleChange}
               ></input>
             </div>
 
@@ -34,6 +129,8 @@ const Apply = () => {
                 name="email"
                 required
                 class="border p-2 rounded"
+             
+                onChange={handleChange}
               ></input>
             </div>
 
@@ -43,10 +140,12 @@ const Apply = () => {
               </label>
               <input
                 type="tel"
-                id="phone"
-                name="phone"
+                id="phoneNumber"
+                name="phoneNumber"
                 required
                 class="border p-2 rounded"
+              
+                onChange={handleChange}
               ></input>
             </div>
 
@@ -56,11 +155,13 @@ const Apply = () => {
               </label>
               <input
                 type="file"
-                id="resume"
-                name="resume"
+                id="file"
+                name="file"
                 accept=".pdf, .doc, .docx"
                 required
                 class="border p-2"
+             
+                 onChange={handleChange}
               ></input>
             </div>
 
@@ -70,23 +171,27 @@ const Apply = () => {
               </label>
               <input
                 type="number"
-                id="experience"
-                name="experience"
+                id="yearsOfExperience"
+                name="yearsOfExperience"
                 min="0"
                 required
                 class="border p-2 rounded"
+             
+                onChange={handleChange}
               ></input>
             </div>
 
             <div class="flex flex-col">
               <label for="additionalInfo" class="mb-1">
-                Additional Information
+                description
               </label>
               <textarea
-                id="additionalInfo"
-                name="additionalInfo"
+                id="description"
+                name="description"
                 rows="4"
+                required
                 class="border p-2 rounded"
+                onChange={handleChange}
               ></textarea>
             </div>
             <button
@@ -96,6 +201,7 @@ const Apply = () => {
               Apply Now
             </button>
           </form>
+          <ToastContainer />
         </div>
       </div>
       <Footer />
