858894340e5d91bd5dac4ee31b744a61d8eb03bc
baudoin <baudoinvicbolingo@gmail.com>
2024-02-27 17:49:06 +0200

    added functionality

--- src/compontents/Footer.jsx
+++ src/compontents/Footer.jsx
@@ -22,6 +22,7 @@ const Footer = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    
 
     try {
       let token = localStorage.getItem("token");
@@ -38,7 +39,7 @@ const Footer = () => {
       });
 
       console.log("Subscription Response Data:", response.data);
-      toast.success("Thank you for subscribing!");
+      toast.success("Thank you for subscribing");
     } catch (error) {
       console.error("Error:", error.response ? error.response.data : error);
       toast.error("Failed to subscribe. Please try again later.");
@@ -47,119 +48,6 @@ const Footer = () => {
 
 
   return (
-    // <footer class="bg-gray-800 p-8">
-    //   <div class="container mx-auto">
-    //     <div class="flex flex-wrap justify-center">
-    //       <div class="w-full md:w-1/2 lg:w-1/4">
-    //         <h2 class="text-white font-bold mb-4">About FabTechHub</h2>
-    //         <nav>
-    //           <ul class="text-blue-500">
-    //             <li>
-    //               <a href="/homepage">Home</a>
-    //             </li>
-    //             <li>
-    //               <a href="/about">About us</a>
-    //             </li>
-    //             <li>
-    //               <a href="/products">Our products</a>
-    //             </li>
-    //             <li>
-    //               <a href="/career">Careers</a>
-    //             </li>
-    //             <li>
-    //               <a href="/contact">Get in touch with us</a>
-    //             </li>
-    //             <li>
-    //               <a href="/ourservices">Our services</a>
-    //             </li>
-    //           </ul>
-    //         </nav>
-    //       </div>
-
-    //       <div class="w-full md:w-1/2 lg:w-1/4">
-    //         <h2 class="text-white font-bold mb-4">Connect with us</h2>
-    //         <nav>
-    //           <ul class="text-blue-500">
-    //             <li>
-    //               <a href="/contact">Contact us</a>
-    //             </li>
-    //             <li>
-    //               <a href="https://www.linkedin.com/in/fabrice-itulamya-masumbuko-044478161/">
-    //                 Contact the owner
-    //               </a>
-    //             </li>
-    //             <li>
-    //               <a href="https://maps.app.goo.gl/3MyetuoWSYdzTF4L8">
-    //                 Our office
-    //               </a>
-    //             </li>
-    //             <li>
-    //               <a href="https://maps.app.goo.gl/xnbi2ChsSfgWUb9s5">
-    //                 Our branch
-    //               </a>
-    //             </li>
-    //             <li>
-    //               <a href="https://maps.app.goo.gl/3MyetuoWSYdzTF4L8">
-    //                 Location
-    //               </a>
-    //             </li>
-    //           </ul>
-    //         </nav>
-    //       </div>
-
-    //       <div class="w-full md:w-1/2 lg:w-1/4">
-    //         <h2 class="text-white font-bold mb-4">Follow us</h2>
-    //         <nav>
-    //           <ul class="flex gap-4">
-    //             <li>
-    //               <a href="https://www.facebook.com/profile.php?id=100088689596878">
-    //                 <CiFacebook class="text-blue-500 text-3xl" />
-    //               </a>
-    //             </li>
-    //             <li>
-    //               <a href="https://www.instagram.com/lepieux_1/">
-    //                 <FaInstagram class="text-blue-500 text-3xl" />
-    //               </a>
-    //             </li>
-    //             <li>
-    //               <a href="https://www.linkedin.com/in/fabrice-itulamya-masumbuko-044478161/">
-    //                 <FaLinkedin class="text-blue-500 text-3xl" />
-    //               </a>
-    //             </li>
-    //           </ul>
-    //         </nav>
-    //       </div>
-
-    //       <div class="w-full md:w-1/2 lg:w-1/4">
-    //         <h2 class="text-white font-bold mb-4">
-    //           Subscribe to Our Newsletter
-    //         </h2>
-    //         <form onClick={handleSubmit} action="#" class="subscription-form">
-    //           <div class="flex items-center">
-    //             <input
-    //               type="text"
-    //               name="email"
-    //               value={email}
-    //               onChange={handleChange}
-    //               id="placenewsletter"
-    //               placeholder="Your email address"
-    //               class="border border-gray-300 rounded-md py-2 px-3 mr-2"
-    //             />
-    //             <button
-    //               type="submit"
-    //               class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-3 rounded-md"
-    //               id="subscription-submit"
-    //             >
-    //               Submit
-    //             </button>
-    //           </div>
-    //         </form>
-    //         <ToastContainer />
-    //       </div>
-    //     </div>
-    //   </div>
-    // </footer>
-
     <div className="Fab-footer">
       <section className="newletter bg-gray-800">
         <div className="container flexSB">
@@ -168,14 +56,25 @@ const Footer = () => {
             <span>To stay updated with our events and information</span>
           </div>
 
-          <div class="flex items-center space-x-2 mt-5 ml-10">
-            <input
-              type="text"
-              placeholder="Enter email address"
-              class="w-80 border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500 text-gray-800"
-            />
-            <i class="fa fa-paper-plane text-gray-500 cursor-pointer"></i>
-          </div>
+        
+          <form onSubmit={handleSubmit} className="subscription-form">
+            <div className="flex items-center space-x-2 mt-5 ml-10">
+              <input
+                type="text"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                required
+                placeholder="Enter email address"
+                className="w-80 border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:border-blue-500 text-gray-800"
+              />
+              <button
+                type="submit"
+                className="fa fa-paper-plane text-gray-900 cursor-pointer bg-white p-3"
+              ></button>
+            </div>
+          </form>
+          <ToastContainer />
         </div>
       </section>
       <footer>
