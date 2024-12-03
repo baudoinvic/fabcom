ce17ad79a395b8424dd266d137ef75a4479e5d78
baudoin <baudoinvicbolingo@gmail.com>
2024-12-03 22:59:23 +0200

    fixing first page

--- src/compontents/Products/WebApp.jsx
+++ src/compontents/Products/WebApp.jsx
@@ -13,7 +13,7 @@ const WebApp = () => {
         </h1>
       </div>
 
-      <div className="flex flex-col lg:flex-row mx-4 md:mx-10 lg:mx-20 xl:mx-32 shadow-md">
+      <div className="flex flex-col lg:flex-row mx-4 md:mx-10 lg:mx-20 xl:mx-32 ">
         <div className="w-full lg:w-1/2 lg:mr-5">
           <span className="mr-4 mt-4">
             The fundamental purpose of all web applications is to facilitate the
