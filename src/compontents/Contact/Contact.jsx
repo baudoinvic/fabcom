d27e5d0f6233012537294e55c7057af468238e51
baudoin <baudoinvicbolingo@gmail.com>
2024-12-06 13:29:46 +0200

    fixed texts

--- src/compontents/Contact/Contact.jsx
+++ src/compontents/Contact/Contact.jsx
@@ -161,7 +161,7 @@ const Contact = () => {
                   rows="4"
                   value={formData.message}
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                  placeholder="Your Message"
+                  placeholder="Share with us your thoughts"
                   onChange={handleChange}
                   required
                 ></textarea>
