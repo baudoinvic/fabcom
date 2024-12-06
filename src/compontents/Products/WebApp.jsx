76c09f0f5e22129556e991ad535604a3b5ab28f5
baudoin <baudoinvicbolingo@gmail.com>
2024-12-06 19:01:28 +0200

    fixed all pages

--- src/compontents/Products/WebApp.jsx
+++ src/compontents/Products/WebApp.jsx
@@ -7,37 +7,41 @@ const WebApp = () => {
     <div>
       <Navbar />
 
-      <div className="mx-4 md:mx-10 lg:mx-20 xl:mx-32">
-        <h1 className="text-left text-4xl font-bold mt-8 mb-4">
+      <div className="pt-16 px-4 md:px-10 mb-32 md:mb-64">
+        <span className="text-purple-800 font-bold text-sm md:text-lg">
           Web Applications
-        </h1>
-      </div>
+        </span>
 
-      <div className="flex flex-col lg:flex-row mx-4 md:mx-10 lg:mx-20 xl:mx-32 ">
-        <div className="w-full lg:w-1/2 lg:mr-5">
-          <span className="mr-4 mt-4">
-            The fundamental purpose of all web applications is to facilitate the
-            completion of one or more tasks. To reiterate the themes of dance
-            and conversation introduced in my last column, truly superior
-            interaction design strikes a delicate balance between the needs and
-            expectations of users and the capabilities and limitations of
-            technology. The ability to consistently find solutions that achieve
-            this balance in a manner appropriate to the medium is a hallmark of
-            an experienced interaction designer.
-          </span>
+        <div className="flex flex-col lg:flex-row mt-8 items-center lg:items-start gap-8">
+          {/* Text Section */}
+          <div className="lg:w-1/2 text-gray-600 space-y-4">
+            <p className="text-sm md:text-base leading-relaxed">
+              The fundamental purpose of all web applications is to facilitate
+              the completion of one or more tasks. To reiterate the themes of
+              dance and conversation introduced in my last column, truly
+              superior interaction design strikes a delicate balance between the
+              needs and expectations of users and the capabilities and
+              limitations of technology. The ability to consistently find
+              solutions that achieve this balance in a manner appropriate to the
+              medium is a hallmark of an experienced interaction designer.
+            </p>
+          </div>
+
+          {/* Image Section */}
+          <div className="relative lg:w-1/2 flex justify-center items-center">
+            <img
+              className="w-full md:w-3/4 lg:w-128 h-60 md:h-80 object-cover rounded-lg shadow-lg border border-solid border-gray-500"
+              src="https://media.istockphoto.com/id/1054541696/vector/online-product-presentation-with-project-team.jpg?s=612x612&w=0&k=20&c=ghVD5anU2Z5NPx1xdqIE6iTVDsNgwe2d3LzTAtaR7zM="
+              alt="Large Background"
+            />
+            <img
+              className="absolute bottom-[-100px] left-[10px] md:bottom-[-150px] md:left-[30px] w-2/5 md:w-1/3 rounded-lg border border-gray-500 shadow-lg"
+              src="https://media.istockphoto.com/id/1054541696/vector/online-product-presentation-with-project-team.jpg?s=612x612&w=0&k=20&c=ghVD5anU2Z5NPx1xdqIE6iTVDsNgwe2d3LzTAtaR7zM="
+              alt="Small Foreground"
+            />
+          </div>
         </div>
-
-        <div className="w-full lg:w-1/2 lg:ml-5">
-          <img
-            src="https://media.istockphoto.com/id/1054541696/vector/online-product-presentation-with-project-team.jpg?s=612x612&w=0&k=20&c=ghVD5anU2Z5NPx1xdqIE6iTVDsNgwe2d3LzTAtaR7zM="
-            alt="web app design"
-            className="w-full h-auto"
-          />
-        </div>
       </div>
-
-      <br />
-      <br />
 
       <Footer />
     </div>
