23e4096d5fa38ec085a08455d84c4bc270992082
baudoin <baudoinvicbolingo@gmail.com>
2024-12-06 12:56:40 +0200

    let's fixit

--- src/compontents/Navbar.jsx
+++ src/compontents/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
             {isAboutDropdownOpen && (
               <div className="bg-gray-700">
                 <Link
-                  to="/provider"
+                  to="/Provider"
                   className="block my-2 px-12 py-3"
                   onClick={toggleModal}
                 >
