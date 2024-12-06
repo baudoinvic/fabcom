bb519247c55f68a68ee9022510f378a5cfa735a6
baudoin <baudoinvicbolingo@gmail.com>
2024-12-06 17:07:46 +0200

    added new style

--- src/compontents/Products/Smart.jsx
+++ src/compontents/Products/Smart.jsx
@@ -4,44 +4,49 @@ import Footer from '../Footer';
 
 const Smart = () => {
   return (
-    <div className="container mx-auto">
+    <div>
       <Navbar />
 
-      <div className="mx-4 md:mx-10 lg:mx-20 xl:mx-32">
-        <h1 className="text-left text-4xl font-bold mt-8 mb-4">
+      <div className="pt-16 px-4 md:px-10 mb-32 md:mb-64">
+        <span className="text-purple-800 font-bold text-sm md:text-lg">
           Smart Integration
-        </h1>
-      </div>
+        </span>
 
-      <div className="flex flex-col lg:flex-row mx-4 md:mx-10 lg:mx-20 xl:mx-32 ">
-        <div className="w-full lg:w-1/2 lg:mr-5">
-          <span className="mr-4 mt-4">
-            "Smart integration" typically refers to the integration of smart
-            technologies or devices into various systems or processes to enhance
-            efficiency, automation, and functionality. The aim of smart
-            integration is to create seamless and intelligent connections
-            between different components, enabling them to work together
-            cohesively and provide added value. This concept is commonly
-            associated with the broader idea of the "Internet of Things" (IoT)
-            and smart systems. Smart Systems Integration as a cross-section
-            technology opens up the possibility to harness physical effects -
-            macro, micro, or nano. This applies to the design of new materials,
-            such as nano-materials (CNTs) or nanopowder, as well as for the use
-            of highly porous materials or functional surfaces.
-          </span>
+        <div className="flex flex-col lg:flex-row mt-8 items-center lg:items-start gap-8">
+          {/* Text Section */}
+          <div className="lg:w-1/2 text-gray-600 space-y-4">
+            <p className="text-sm md:text-base leading-relaxed">
+              Smart integration typically refers to the integration of smart
+              technologies or devices into various systems or processes to
+              enhance efficiency, automation, and functionality. The aim of
+              smart integration is to create seamless and intelligent
+              connections between different components, enabling them to work
+              together cohesively and provide added value. This concept is
+              commonly associated with the broader idea of the "Internet of
+              Things" (IoT) and smart systems. Smart Systems Integration as a
+              cross-section technology opens up the possibility to harness
+              physical effects - macro, micro, or nano. This applies to the
+              design of new materials, such as nano-materials (CNTs) or
+              nanopowder, as well as for the use of highly porous materials or
+              functional surfaces.
+            </p>
+          </div>
+
+          {/* Image Section */}
+          <div className="relative lg:w-1/2 flex justify-center items-center">
+            <img
+              className="w-full md:w-3/4 lg:w-128 h-60 md:h-80 object-cover rounded-lg shadow-lg border border-solid border-gray-500"
+              src="https://www.masstrans.in/wp-content/uploads/automatic-fare-collection-system-in-bus.jpg"
+              alt="Large Background"
+            />
+            <img
+              className="absolute bottom-[-100px] left-[10px] md:bottom-[-150px] md:left-[30px] w-2/5 md:w-1/3 rounded-lg border border-gray-500 shadow-lg"
+              src="https://www.masstrans.in/wp-content/uploads/automatic-fare-collection-system-in-bus.jpg"
+              alt="Small Foreground"
+            />
+          </div>
         </div>
-
-        <div className="w-full lg:w-1/2 lg:ml-5">
-          <img
-            src="https://www.iotallknow.com/wp-content/uploads/2022/11/%E6%99%BA%E8%83%BD%E7%81%8C%E6%BA%89%E7%9A%84%E5%BA%94%E7%94%A8%E5%AE%9E%E4%BE%8B.webp"
-            alt="Smart Integration"
-            className="w-full h-auto"
-          />
-        </div>
       </div>
-
-      <br />
-      <br />
 
       <Footer />
     </div>
