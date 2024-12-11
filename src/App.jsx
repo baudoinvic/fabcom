3cc67c6792d5f4f60195571f71da44c176b9815e
baudoin <baudoinvicbolingo@gmail.com>
2024-12-11 11:35:07 +0200

    debug it

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import Navbar from "./compontents/Navbar";
-import About from "./compontents/About/About";
 import Service from "./compontents/Service";
 import Description from "./compontents/Description";
 import Footer from "./compontents/Footer";
